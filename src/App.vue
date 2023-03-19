5d1785fe07bd78b496e79fe1013310720b20709b
malt9830 <malt9830@stud.kea.dk>
2023-03-19 01:28:23 +0100

    Adding dist

--- src/App.vue
+++
@@ -1,4 +0,0 @@
-<template>
-  <Header/>
-  <Game/>
-</template>
--- src/components/Header.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <header class="bg-zinc-900 py-4 px-8">
-    <h1 class="text-3xl">Soulsborndle</h1>
-  </header>
-</template>

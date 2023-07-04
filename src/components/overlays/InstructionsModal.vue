46bde3be14172cdd6d019915f83a419d84b17ade
malt9830 <malt9830@stud.kea.dk>
2023-07-04 14:34:53 +0200

    Modals hide only when background is clicked

--- src/components/overlays/InstructionsModal.vue
+++ src/components/overlays/InstructionsModal.vue
@@ -1,8 +1,8 @@
 <template>
   <Teleport to="body">
     <Transition name="fade">
-      <div v-if="showInstructions" @click="$emit('hideInstructions')" class="fixed top-0 w-full h-screen bg-black bg-opacity-30 flex flex-col place-content-center cursor-pointer p-2">
-        <div class="w-full max-w-2xl mx-auto bg-black p-4 text-center md:text-lg">
+      <div v-if="showInstructions" @click.self="$emit('hideInstructions')" class="fixed top-0 w-full h-screen bg-black bg-opacity-30 flex flex-col place-content-center cursor-pointer p-2">
+        <div class="w-full max-w-lg mx-auto bg-black p-4 text-center md:text-lg cursor-auto">
           <p class="text-3xl mb-2">Instructions</p>
           <p class="mb-2">Enter the name a boss and narrow it down using the hints based on your previous guesses.</p>
           <p>The bosses are from the following games:</p>
@@ -27,7 +27,7 @@ const games = ref(['Demon\'s Souls', 'Dark Souls', 'Dark Souls II', 'Dark Souls 
 <style scoped>
 .fade-enter-active,
 .fade-leave-active {
-  transition: opacity 250ms ease;
+  transition: opacity 300ms ease;
 }
 
 .fade-enter-from,
--- src/components/overlays/RecordsModal.vue
+++ src/components/overlays/RecordsModal.vue
@@ -1,24 +1,27 @@
 <template>
   <Teleport to="body">
     <Transition name="fade">
-      <div v-if="showRecords" @click="$emit('hideRecords')" class="fixed top-0 w-full h-screen bg-black bg-opacity-30 flex flex-col place-content-center cursor-pointer p-2">
-        <div class="w-full max-w-xl mx-auto bg-black p-4 text-center md:text-lg">
+      <div v-if="showRecords" @click.self="$emit('hideRecords')" class="fixed top-0 w-full h-screen bg-black bg-opacity-30 flex flex-col place-content-center cursor-pointer p-2">
+        <div class="w-full max-w-lg mx-auto bg-black p-4 text-center cursor-auto md:text-lg">
           <p class="text-3xl">Records</p>
           <p v-if="records.length === 0">You have not guessed a boss yet.</p>
-          <table v-else class="w-full text-left border-separate border-spacing-2">
-            <thead>
-              <tr>
-                <th>#</th>
-                <th>Boss</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="record in records" :key="`${record.guesses}-${record.name}`">
-                <td>{{ record.guesses }}</td>
-                <td>{{ record.name }}</td>
-              </tr>
-            </tbody>
-          </table>
+          <div v-else>
+            <p>Listed below is your 10 fastest guessed bosses sorted by amount of guesses.</p>
+            <table class="w-full text-left border-separate border-spacing-2">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Boss</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="record in records" :key="`${record.guesses}-${record.name}`">
+                  <td>{{ record.guesses }}</td>
+                  <td>{{ record.name }}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </Transition>
@@ -38,7 +41,7 @@ const props = defineProps({
 <style scoped>
 .fade-enter-active,
 .fade-leave-active {
-  transition: opacity 250ms ease;
+  transition: opacity 300ms ease;
 }
 
 .fade-enter-from,

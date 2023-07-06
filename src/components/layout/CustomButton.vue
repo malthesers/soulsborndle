1995483750f16dc91a1a6960b7cd6469d6fbdb5b
malt9830 <malt9830@stud.kea.dk>
2023-07-07 00:58:31 +0200

    Button uses props as displayed text

--- src/components/layout/CustomButton.vue
+++ src/components/layout/CustomButton.vue
@@ -1,3 +1,9 @@
 <template>
-  <button class="font-bold border-2 border-white rounded text-xl mr-4 mb-4 py-2 px-4 duration-200 hover:bg-white hover:text-zinc-900"></button>
+  <button class="font-bold border-2 border-white rounded text-xl mr-4 mb-4 py-2 px-4 duration-200 hover:bg-white hover:text-zinc-900">{{ text }}</button>
 </template>
+
+<script setup>
+const props = defineProps({
+  text: String
+})
+</script>

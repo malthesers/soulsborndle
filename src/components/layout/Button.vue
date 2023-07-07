ce3283d11e1a7a738d3fa6e716f9318ae6af35cf
malt9830 <malt9830@stud.kea.dk>
2023-07-08 00:18:56 +0200

    Removed margin from button component

--- src/components/layout/Button.vue
+++ src/components/layout/Button.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="font-bold border-2 border-white rounded text-xl mr-4 mb-4 py-2 px-4 duration-200 hover:bg-white hover:text-zinc-900">{{ text }}</button>
+  <button class="font-bold border-2 border-white rounded text-xl py-2 px-4 duration-200 hover:bg-white hover:text-zinc-900">{{ text }}</button>
 </template>
 
 <script setup>

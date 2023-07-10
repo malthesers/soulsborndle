0759ca7dca0b5dfd12ba543692d407918f5c332e
malt9830 <malt9830@stud.kea.dk>
2023-07-10 17:12:48 +0200

    Added yharnam-layer to minimize tw-repitition

--- src/components/layout/YharnamScreen.vue
+++ src/components/layout/YharnamScreen.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="fixed -z-10 top-0 w-full h-screen filter brightness-50">
-    <div class="absolute top-0 w-full h-full bg-cover bg-[20%_center] bg-[url('/1_moon.svg')]"></div>
-    <div class="absolute top-0 w-full h-full bg-cover bg-[5%_center] bg-[url('/2_background.svg')]"></div>
-    <div class="absolute top-0 w-full h-full bg-cover bg-center bg-[url('/3_bg-light.svg')]"></div>
-    <div class="absolute top-0 w-full h-full bg-cover bg-[30%_center] bg-[url('/4_middleground.svg')]"></div>
-    <div class="absolute top-0 w-full h-full bg-cover bg-center bg-[url('/5_mg-light.svg')]"></div>
-    <div class="absolute top-0 w-full h-full bg-cover bg-[10%_center] bg-[url('/6_foreground.svg')]"></div>
+    <div class="yharnam-layer bg-[20%_center] bg-[url('/1_moon.svg')]"></div>
+    <div class="yharnam-layer bg-[5%_center] bg-[url('/2_background.svg')]"></div>
+    <div class="yharnam-layer bg-center bg-[url('/3_bg-light.svg')]"></div>
+    <div class="yharnam-layer bg-[30%_center] bg-[url('/4_middleground.svg')]"></div>
+    <div class="yharnam-layer bg-center bg-[url('/5_mg-light.svg')]"></div>
+    <div class="yharnam-layer bg-[10%_center] bg-[url('/6_foreground.svg')]"></div>
   </div>
 </template>

c25c91dadf7b27cbacf94c6e11b11831be36e70a
malt9830 <malt9830@stud.kea.dk>
2023-07-10 17:22:41 +0200

    Moved background layers from public to assets

--- src/components/layout/YharnamScreen.vue
+++ src/components/layout/YharnamScreen.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="fixed -z-10 top-0 w-full h-screen filter brightness-50">
-    <div class="yharnam-layer bg-[20%_center] bg-[url('/1_moon.svg')]"></div>
-    <div class="yharnam-layer bg-[5%_center] bg-[url('/2_background.svg')]"></div>
-    <div class="yharnam-layer bg-center bg-[url('/3_bg-light.svg')]"></div>
-    <div class="yharnam-layer bg-[30%_center] bg-[url('/4_middleground.svg')]"></div>
-    <div class="yharnam-layer bg-center bg-[url('/5_mg-light.svg')]"></div>
-    <div class="yharnam-layer bg-[10%_center] bg-[url('/6_foreground.svg')]"></div>
+    <div class="yharnam-layer bg-[20%_center] bg-[url('/src/assets/background/1_moon.svg')]"></div>
+    <div class="yharnam-layer bg-[5%_center] bg-[url('/src/assets/background/2_background.svg')]"></div>
+    <div class="yharnam-layer bg-center bg-[url('/src/assets/background/3_bg-light.svg')]"></div>
+    <div class="yharnam-layer bg-[30%_center] bg-[url('/src/assets/background/4_middleground.svg')]"></div>
+    <div class="yharnam-layer bg-center bg-[url('/src/assets/background/5_mg-light.svg')]"></div>
+    <div class="yharnam-layer bg-[10%_center] bg-[url('/src/assets/background/6_foreground.svg')]"></div>
   </div>
 </template>

1875cdae91ed7277446d4564ae70e3d852d7c950
malt9830 <malt9830@stud.kea.dk>
2023-07-11 16:41:21 +0200

    Added World 1 DeS bosses

--- src/bosses/demons-souls.js
+++ src/bosses/demons-souls.js
@@ -5,7 +5,31 @@ export const bosses = [
     health: 1150,
     souls: 1270,
     weaknesses: ['fire'],
+    resistances: ['strike']
+  },
+  {
+    name: 'Tower Knight',
+    game: "Demon's Souls",
+    health: 1365,
+    souls: 8570,
+    weaknesses: ['magic'],
     resistances: []
+  },
+  {
+    name: 'Penetrator',
+    game: "Demon's Souls",
+    health: 2960,
+    souls: 35850,
+    weaknesses: ['magic', 'strike'],
+    resistances: []
+  },
+  {
+    name: 'Old King Allant',
+    game: "Demon's Souls",
+    health: 2960,
+    souls: 35850,
+    weaknesses: ['fire'],
+    resistances: ['magic']
   }
 ]
 

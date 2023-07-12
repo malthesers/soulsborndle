6e8a96ee17ac9e759e913032d7aacc2ff5d2afc6
malt9830 <malt9830@stud.kea.dk>
2023-07-12 15:09:03 +0200

    Added Dark Souls 1 bosses A-C

--- src/bosses/dark-souls-1.js
+++ src/bosses/dark-souls-1.js
@@ -6,6 +6,54 @@ export const bosses = [
     souls: 2000,
     weaknesses: ['fire', 'slash'],
     resistances: []
+  },
+  {
+    name: 'Bell Gargoyle',
+    game: 'Dark Souls',
+    health: 1000,
+    souls: 10000,
+    weaknesses: ['fire', 'lightning'],
+    resistances: []
+  },
+  {
+    name: 'Capra Demon',
+    game: 'Dark Souls',
+    health: 1176,
+    souls: 6000,
+    weaknesses: ['fire'],
+    resistances: []
+  },
+  {
+    name: 'Ceaseless Discharge',
+    game: 'Dark Souls',
+    health: 4200,
+    souls: 20000,
+    weaknesses: [],
+    resistances: []
+  },
+  {
+    name: 'Centipede Demon',
+    game: 'Dark Souls',
+    health: 3432,
+    souls: 40000,
+    weaknesses: ['lightning'],
+    resistances: []
+  },
+  {
+    name: 'Chaos Witch Quelaag',
+    game: 'Dark Souls',
+    health: 3139,
+    souls: 20000,
+    weaknesses: ['lightning'],
+    resistances: ['fire']
+  },
+  {
+    name: 'Crossbreed Priscilla',
+    game: 'Dark Souls',
+    health: 2300,
+    souls: 30000,
+    weaknesses: ['fire'],
+    resistances: []
   }
 ]
 

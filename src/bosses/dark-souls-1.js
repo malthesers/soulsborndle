23291f425467d0b6d4a40e5689d1ee06dca914f9
malt9830 <malt9830@stud.kea.dk>
2023-07-12 15:26:33 +0200

    Added Dark Souls 1 bosses D-I

--- src/bosses/dark-souls-1.js
+++ src/bosses/dark-souls-1.js
@@ -28,7 +28,7 @@ export const bosses = [
     game: 'Dark Souls',
     health: 4200,
     souls: 20000,
-    weaknesses: [],
+    weaknesses: ['magic'],
     resistances: ['fire']
   },
   {
@@ -36,7 +36,7 @@ export const bosses = [
     game: 'Dark Souls',
     health: 3432,
     souls: 40000,
-    weaknesses: ['lightning'],
+    weaknesses: ['magic', 'lightning'],
     resistances: ['fire']
   },
   {
@@ -53,6 +53,62 @@ export const bosses = [
     health: 2300,
     souls: 30000,
     weaknesses: ['fire', 'lightning'],
+    resistances: []
+  },
+  {
+    name: 'Dark Sun Gwyndolin',
+    game: 'Dark Souls',
+    health: 2000,
+    souls: 40000,
+    weaknesses: ['fire'],
+    resistances: []
+  },
+  {
+    name: 'Demon Firesage',
+    game: 'Dark Souls',
+    health: 5950,
+    souls: 20000,
+    weaknesses: ['lightning'],
+    resistances: []
+  },
+  {
+    name: 'Four Kings',
+    game: 'Dark Souls',
+    health: 9604,
+    souls: 60000,
+    weaknesses: ['fire', 'lightning'],
+    resistances: []
+  },
+  {
+    name: 'Gaping Dragon',
+    game: 'Dark Souls',
+    health: 4401,
+    souls: 25000,
+    weaknesses: ['lightning'],
+    resistances: []
+  },
+  {
+    name: 'Great Grey Wolf Sif',
+    game: 'Dark Souls',
+    health: 3432,
+    souls: 40000,
+    weaknesses: ['fire'],
+    resistances: []
+  },
+  {
+    name: 'Gwyn Lord of Cinder',
+    game: 'Dark Souls',
+    health: 4250,
+    souls: 70000,
+    weaknesses: ['fire'],
+    resistances: ['lightning']
+  },
+  {
+    name: 'Iron Golem',
+    game: 'Dark Souls',
+    health: 2880,
+    souls: 40000,
+    weaknesses: [],
     resistances: []
   }
 ]

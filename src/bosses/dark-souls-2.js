6e1f1cfffdb948e0182440f129e0b9c28d40521e
malt9830 <malt9830@stud.kea.dk>
2023-07-13 20:11:34 +0200

    Added DS2 bosses up to rats, rats, we are the rats

--- src/bosses/dark-souls-2.js
+++ src/bosses/dark-souls-2.js
@@ -54,6 +54,54 @@ export const bosses = [
     souls: 15000,
     weaknesses: ['fire', 'strike'],
     resistances: ['magic', 'thrust']
+  },
+  {
+    name: 'Flexible Sentry',
+    game: 'Dark Souls II',
+    health: 3150,
+    souls: 14000,
+    weaknesses: ['lightning'],
+    resistances: ['fire']
+  },
+  {
+    name: 'Lost Sinner',
+    game: 'Dark Souls II',
+    health: 3560,
+    souls: 45000,
+    weaknesses: ['magic'],
+    resistances: ['fire']
+  },
+  {
+    name: 'Belfry Gargoyles',
+    game: 'Dark Souls II',
+    health: 1400,
+    souls: 25000,
+    weaknesses: ['magic', 'strike'],
+    resistances: []
+  },
+  {
+    name: 'Ruin Sentinel',
+    game: 'Dark Souls II',
+    health: 2330,
+    souls: 33000,
+    weaknesses: ['lightning', 'strike'],
+    resistances: []
+  },
+  {
+    name: 'Royal Rat Vanguard',
+    game: 'Dark Souls II',
+    health: 1410,
+    souls: 11000,
+    weaknesses: ['fire'],
+    resistances: []
+  },
+  {
+    name: 'Royal Rat Authority',
+    game: 'Dark Souls II',
+    health: 4330,
+    souls: 14000,
+    weaknesses: ['fire'],
+    resistances: []
   }
 ]
 

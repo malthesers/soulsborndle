d1d316c41effe39cf59ce7bd04b6dd9135397a21
malt9830 <malt9830@stud.kea.dk>
2023-07-13 20:04:14 +0200

    Added DS2 bosses up to Skeleton Lords

--- src/bosses/dark-souls-2.js
+++ src/bosses/dark-souls-2.js
@@ -6,6 +6,54 @@ export const bosses = [
     souls: 10000,
     weaknesses: ['dark'],
     resistances: []
+  },
+  {
+    name: 'The Pursuer',
+    game: 'Dark Souls II',
+    health: 3110,
+    souls: 17000,
+    weaknesses: ['lightning', 'strike'],
+    resistances: ['dark']
+  },
+  {
+    name: 'Executioners Chariot',
+    game: 'Dark Souls II',
+    health: 4140,
+    souls: 19000,
+    weaknesses: ['fire', 'lightning'],
+    resistances: ['dark', 'strike']
+  },
+  {
+    name: 'Looking Glass Knight',
+    game: 'Dark Souls II',
+    health: 6540,
+    souls: 25500,
+    weaknesses: ['magic', 'dark', 'strike'],
+    resistances: ['lightning']
+  },
+  {
+    name: 'Skeleton Lords (Scythe Lord)',
+    game: 'Dark Souls II',
+    health: 780,
+    souls: 15000,
+    weaknesses: ['fire', 'strike'],
+    resistances: ['magic', 'thrust']
+  },
+  {
+    name: 'Skeleton Lords (Halberd Lord)',
+    game: 'Dark Souls II',
+    health: 780,
+    souls: 15000,
+    weaknesses: ['fire', 'strike'],
+    resistances: ['magic', 'thrust']
+  },
+  {
+    name: 'Skeleton Lords (Staff Lord)',
+    game: 'Dark Souls II',
+    health: 520,
+    souls: 15000,
+    weaknesses: ['fire', 'strike'],
+    resistances: ['magic', 'thrust']
   }
 ]
 

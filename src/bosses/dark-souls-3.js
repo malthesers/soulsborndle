b65ab78571c2e5ca5c8c223b382ddb2038e5ffb3
malt9830 <malt9830@stud.kea.dk>
2023-07-14 21:13:03 +0200

    Added DS3 bosses up to Old Demon King

--- src/bosses/dark-souls-3.js
+++ src/bosses/dark-souls-3.js
@@ -6,6 +6,62 @@ export const bosses = [
     souls: 3000,
     weaknesses: ['fire'],
     resistances: ['dark']
+  },
+  {
+    name: 'Vordt of the Boreal Valley',
+    game: 'Dark Souls III',
+    health: 1328,
+    souls: 3000,
+    weaknesses: ['fire', 'dark'],
+    resistances: ['slash', 'thrust']
+  },
+  {
+    name: 'Curse-Rotted Greatwood',
+    game: 'Dark Souls III',
+    health: 5405,
+    souls: 7000,
+    weaknesses: ['fire', 'slash', 'thrust'],
+    resistances: []
+  },
+  {
+    name: 'Crystal Sage',
+    game: 'Dark Souls III',
+    health: 2723,
+    souls: 8000,
+    weaknesses: ['strike', 'slash', 'thrust'],
+    resistances: ['magic']
+  },
+  {
+    name: 'Abyss Watchers',
+    game: 'Dark Souls III',
+    health: 1548,
+    souls: 18000,
+    weaknesses: ['lightning'],
+    resistances: ['dark']
+  },
+  {
+    name: 'Deacons of the Deep',
+    game: 'Dark Souls III',
+    health: 4099,
+    souls: 13000,
+    weaknesses: ['strike', 'slash', 'thrust'],
+    resistances: ['magic', 'dark']
+  },
+  {
+    name: 'High Lord Wolnir',
+    game: 'Dark Souls III',
+    health: 15041,
+    souls: 22000,
+    weaknesses: [],
+    resistances: ['dark']
+  },
+  {
+    name: 'Old Demon King',
+    game: 'Dark Souls III',
+    health: 5301,
+    souls: 25000,
+    weaknesses: ['dark'],
+    resistances: ['fire']
   }
 ]
 

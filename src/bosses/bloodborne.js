0ee92c702a30cc8bf93a4bd71ba97f8f70195422
malt9830 <malt9830@stud.kea.dk>
2023-07-15 03:32:38 +0200

    Added BB bosses A-F

--- src/bosses/bloodborne.js
+++ src/bosses/bloodborne.js
@@ -1,10 +1,50 @@
 export const bosses = [
   {
+    name: 'Amygdala',
+    game: 'Bloodborne',
+    health: 6404,
+    souls: 21000,
+    weaknesses: ['magic', 'fire', 'lightning'],
+    resistances: ['strike']
+  },
+  {
+    name: 'Blood-starved Beast',
+    game: 'Bloodborne',
+    health: 3470,
+    souls: 6600,
+    weaknesses: ['fire'],
+    resistances: []
+  },
+  {
+    name: 'Celestial Emissary',
+    game: 'Bloodborne',
+    health: 2764,
+    souls: 24640,
+    weaknesses: ['thrust', 'lightning'],
+    resistances: ['physical', 'strike']
+  },
+  {
     name: 'Cleric Beast',
     game: 'Bloodborne',
     health: 3015,
     souls: 4000,
     weaknesses: ['fire'],
+    resistances: ['strike']
+  },
+  {
+    name: 'Darkbeast Paarl',
+    game: 'Bloodborne',
+    health: 4552,
+    souls: 21000,
+    weaknesses: ['magic', 'fire'],
+    resistances: ['strike', 'lightning']
+  },
+  {
+    name: 'Ebrietas, Daughter of the Cosmos',
+    game: 'Bloodborne',
+    health: 12493,
+    souls: 36300,
+    weaknesses: ['lightning', 'thrust'],
     resistances: ['strike']
   },
   {
@@ -24,28 +64,12 @@ export const bosses = [
     resistances: []
   },
   {
-    name: 'Blood-starved Beast',
-    game: 'Bloodborne',
-    health: 3470,
-    souls: 6600,
-    weaknesses: ['fire'],
-    resistances: []
-  },
-  {
     name: 'The Witch of Hemwick',
     game: 'Bloodborne',
     health: 2611,
     souls: 11800,
     weaknesses: [],
     resistances: []
-  },
-  {
-    name: 'Darkbeast Paarl',
-    game: 'Bloodborne',
-    health: 4552,
-    souls: 21000,
-    weaknesses: ['magic', 'fire'],
-    resistances: ['strike', 'lightning']
   },
   {
     name: 'Vicar Amelia',

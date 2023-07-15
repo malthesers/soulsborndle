c5c4f13184d5b6fba37d9635a2b6e80ea0c96103
malt9830 <malt9830@stud.kea.dk>
2023-07-15 04:02:33 +0200

    Removed name from KnownInfo.vue

--- src/components/panels/KnownInfo.vue
+++ src/components/panels/KnownInfo.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="lg:sticky top-0 h-max sm:flex justify-center lg:block text-center bg-zinc-900 bg-opacity-60 p-2">
-      <!-- <p class="text-3xl p-2">{{ known.name }}</p> -->
       <div class="sm:inline-block lg:block text-2xl mx-4"><span class="text-sm">Game</span><p>{{ known.game }}</p></div>
       <div class="sm:inline-block lg:block text-2xl mx-4"><span class="text-sm">Health</span><p v-if="known.health">{{ known.health.toLocaleString() }}</p><p v-else class="whitespace-nowrap">{{ known.healthMin.toLocaleString() }} - {{ known.healthMax.toLocaleString() }}</p></div>
       <div class="sm:inline-block lg:block text-2xl mx-4"><span class="text-sm">Souls</span><p v-if="known.souls">{{ known.souls.toLocaleString() }}</p><p v-else class="whitespace-nowrap">{{ known.soulsMin.toLocaleString() }} - {{ known.soulsMax.toLocaleString() }}</p></div>

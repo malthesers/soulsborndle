62848430c84f1d285f594cb48bfd63f5016f8396
malt9830 <malt9830@stud.kea.dk>
2023-07-16 15:59:26 +0200

    Added ER main bosses - last 8

--- src/bosses/elden-ring.js
+++ src/bosses/elden-ring.js
@@ -62,6 +62,94 @@ export const bosses = [
     souls: 480000,
     weaknesses: ['fire'],
     resistances: ['holy']
+  },
+  {
+    name: 'Lichdragon Fortissax',
+    game: 'Elden Ring',
+    health: 12903,
+    souls: 90000,
+    weaknesses: ['thrust'],
+    resistances: ['lightning', 'holy']
+  },
+  {
+    name: 'Beast Clergyman',
+    game: 'Elden Ring',
+    health: 10620,
+    souls: 220000,
+    weaknesses: [],
+    resistances: ['holy']
+  },
+  {
+    name: 'Maliketh, The Black Blade',
+    game: 'Elden Ring',
+    health: 10620,
+    souls: 220000,
+    weaknesses: [],
+    resistances: ['holy']
+  },
+  {
+    name: 'Astel, Naturalborn of the Void',
+    game: 'Elden Ring',
+    health: 11170,
+    souls: 80000,
+    weaknesses: [],
+    resistances: []
+  },
+  {
+    name: 'Regal Ancestor Spirit',
+    game: 'Elden Ring',
+    health: 6301,
+    souls: 24000,
+    weaknesses: ['fire', 'holy', 'slash'],
+    resistances: ['magic']
+  },
+  {
+    name: 'Radagon of the Golden Order',
+    game: 'Elden Ring',
+    health: 13339,
+    souls: 500000,
+    weaknesses: ['strike'],
+    resistances: ['holy']
+  },
+  {
+    name: 'Elden Beast',
+    game: 'Elden Ring',
+    health: 22127,
+    souls: 500000,
+    weaknesses: [],
+    resistances: ['holy']
+  },
+  {
+    name: 'Godfrey, First Elden Lord',
+    game: 'Elden Ring',
+    health: 21903,
+    souls: 300000,
+    weaknesses: [],
+    resistances: ['magic', 'fire', 'holy']
+  },
+  {
+    name: 'Hoarah Loux, Warrior',
+    game: 'Elden Ring',
+    health: 21903,
+    souls: 300000,
+    weaknesses: ['slash'],
+    resistances: ['holy']
+  },
+  {
+    name: 'Fire Giant',
+    game: 'Elden Ring',
+    health: 42363,
+    souls: 180000,
+    weaknesses: ['slash'],
+    resistances: ['fire']
+  },
+  {
+    name: 'Dragonlord Placidusax',
+    game: 'Elden Ring',
+    health: 26651,
+    souls: 210000,
+    weaknesses: ['thrust'],
+    resistances: []
   }
 ]
 

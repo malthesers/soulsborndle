9bfe65e86e432c957cf5b272cb5de44b9fd16f05
malt9830 <malt9830@stud.kea.dk>
2023-07-19 23:51:18 +0200

    Bug fix: Firefox not displaying damage icons
    grid + place-content: center caused damage type icons to be 0x0 dimensions on Firefox only, which has now been fixed with margin: auto on the icons instead of place-content: center on container

--- src/components/layout/DamageBox.vue
+++ src/components/layout/DamageBox.vue
@@ -1,6 +1,6 @@
 <template>
-  <span class="inline-grid place-content-center w-11 h-11 border-zinc-900 border-4 rounded shadow-inner-dmg">
-    <img class="w-full p-1 object-cover" :src="`icons/${damageType}.svg`">
+  <span class="grid w-11 h-11 border-zinc-900 border-4 rounded shadow-inner-dmg">
+    <img class="w-full m-auto p-1 object-cover" :src="`icons/${damageType}.svg`">
   </span>
 </template>
 

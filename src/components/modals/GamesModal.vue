d3f53df82ef1168119204a8a15a44c380142f70a
malt9830 <malt9830@stud.kea.dk>
2023-07-19 03:42:57 +0200

    Game is now reset after changing games

--- src/components/modals/GamesModal.vue
+++ src/components/modals/GamesModal.vue
@@ -5,7 +5,7 @@
         <div class="w-full max-w-xl mx-auto bg-black p-4 text-center cursor-auto md:text-lg">
           <p class="text-3xl">Games</p>
           <p class="mb-2">Below you can choose the games whose bosses will be included amongst the possible bosses.</p>
-          <div class="grid xs:grid-cols-2 gap-4">
+          <div class="grid xs:grid-cols-2 gap-4 mb-4">
             <Button v-for="(game, key) in chosenGames"
               :key="game.name"
               :text="game.name"
@@ -14,6 +14,7 @@
               @click="chosenGames[key].isChosen = !chosenGames[key].isChosen"
             />
           </div>
+          <Button @click="$emit('hideGames')" text="New Game"/>
         </div>
       </div>
     </Transition>

fc0636d37969f5a236ac316c00e140f43309289b
malt9830 <malt9830@stud.kea.dk>
2023-07-19 03:20:46 +0200

    Restructured GamesModal logic to object loop

--- src/components/modals/GamesModal.vue
+++ src/components/modals/GamesModal.vue
@@ -6,12 +6,17 @@
           <p class="text-3xl">Games</p>
           <p class="mb-2">Below you can choose the games whose bosses will be included amongst the possible bosses.</p>
           <div class="grid xs:grid-cols-2 gap-4">
-            <Button @click="chosenGames.demonsSouls = !chosenGames.demonsSouls" text="Demon's Souls" :class="[chosenGames.demonsSouls ? 'bg-green-900' : 'bg-red-900']"/>
+            <!-- <Button @click="chosenGames.demonsSouls = !chosenGames.demonsSouls" text="Demon's Souls" :class="[chosenGames.demonsSouls ? 'bg-green-900' : 'bg-red-900']"/>
             <Button @click="chosenGames.darkSouls1 = !chosenGames.darkSouls1" text="Dark Souls" :class="[chosenGames.darkSouls1 ? 'bg-green-900' : 'bg-red-900']"/>
             <Button @click="chosenGames.bloodborne = !chosenGames.bloodborne" text="Bloodborne" :class="[chosenGames.bloodborne ? 'bg-green-900' : 'bg-red-900']"/>
             <Button @click="chosenGames.darkSouls2 = !chosenGames.darkSouls2" text="Dark Souls 2" :class="[chosenGames.darkSouls2 ? 'bg-green-900' : 'bg-red-900']"/>
             <Button @click="chosenGames.eldenRing = !chosenGames.eldenRing" text="Elden Ring" :class="[chosenGames.eldenRing ? 'bg-green-900' : 'bg-red-900']"/>
-            <Button @click="chosenGames.darkSouls3 = !chosenGames.darkSouls3" text="Dark Souls 3" :class="[chosenGames.darkSouls3 ? 'bg-green-900' : 'bg-red-900']"/>
+            <Button @click="chosenGames.darkSouls3 = !chosenGames.darkSouls3" text="Dark Souls 3" :class="[chosenGames.darkSouls3 ? 'bg-green-900' : 'bg-red-900']"/> -->
+            <Button v-for="(isChosen, name) in chosenGames" :key="name"
+              :text="name"
+              :class="[isChosen ? 'bg-green-900' : 'bg-red-900']"
+              @click="chosenGames[name] = !chosenGames[name]"
+            />
           </div>
         </div>
       </div>

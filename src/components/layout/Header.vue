686af51f1e17690631675e9319d5b6cbb20fcfa3
malt9830 <malt9830@stud.kea.dk>
2023-07-20 20:17:04 +0200

    Adjusted header padding

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <header>
-    <div class="bg-zinc-900 py-4 px-8">
+    <div class="bg-zinc-900 py-4 px-6">
       <h1 class="text-2xl sm:text-3xl text-left">Soulsborndle</h1>
     </div>
     <GateOrnament class="transform -scale-y-100"/>

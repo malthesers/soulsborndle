b7fece83e8f120bd471c9598b6a204613587f4a6
malt9830 <malt9830@stud.kea.dk>
2023-07-24 01:55:56 +0200

    Brightened drop-shadow-red

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ module.exports = {
         'inner-dmg': 'inset 0px 0px 5px 5px rgba(0, 0, 0, 0.7)'
       },
       dropShadow: {
-        red: '0 0 5px rgba(239, 68, 68, 0.25)'
+        red: '0 1px 5px rgba(239, 68, 68, 1)'
       }
     },
     screens: {

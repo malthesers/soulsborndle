4f43ab4ae23cc425b46cb6c8d4c93470948f8f04
malt9830 <malt9830@stud.kea.dk>
2023-07-25 01:16:13 +0200

    Fixed records table for mobile

--- src/components/modals/RecordsModal.vue
+++ src/components/modals/RecordsModal.vue
@@ -15,14 +15,21 @@
                   <th v-for="(game, key) in games" :key="key" class="hidden xs:table-cell">{{ game }}</th>
                 </tr>
               </thead>
-              <tbody>
-                <tr v-for="record in records" :key="`${record.guesses}-${record.name}`">
+              <tbody v-for="record in records" :key="`${record.guesses}-${record.name}`">
+                <tr>
                   <td>{{ record.guesses }}</td>
-                  <td>{{ record.name }}</td>
+                  <td class="hidden xs:table-cell" colspan="1">{{ record.name }}</td>
+                  <td class="xs:hidden" colspan="5">{{ record.name }}</td>
+                  <td v-for="(game, key) in games" :key="key"
+                    :class="[record.games[key].isChosen ? 'bg-green-900' : 'bg-red-900']"
+                    class="hidden xs:table-cell"
+                  ></td>
+                </tr>
+                <tr class="xs:hidden">
                   <td
                     v-for="(game, key) in games" :key="key"
                     :class="[record.games[key].isChosen ? 'bg-green-900' : 'bg-red-900']"
-                  ><span class="xs:hidden">{{ game }}</span></td>
+                  ><span>{{ game }}</span></td>
                 </tr>
               </tbody>
             </table>

702f77682b8f0c71240d4199134c790489178fa5
malt9830 <malt9830@stud.kea.dk>
2023-07-25 02:04:38 +0200

    Fixed bug changing games in records

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -122,7 +122,7 @@ function validateGuess(boss) {
     wasGuessed.value = true;
 
     // Add to records
-    updateRecords(boss.name, guessedBosses.value.length, chosenGames.value);
+    updateRecords(boss.name, guessedBosses.value.length, JSON.parse(JSON.stringify(chosenGames.value)));
   } else {
 
     // Validate name

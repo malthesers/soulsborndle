46075199720551a605ad35d14eb186afc80af1f8
malt9830 <malt9830@stud.kea.dk>
2023-07-27 01:42:30 +0200

    Added potential guessed boss transition

--- src/components/panels/BossCardGuess.vue
+++ src/components/panels/BossCardGuess.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <p class="bg-zinc-900 text-lg px-2 py-1 text-center">{{ boss.name }}</p>
+    <p class="bg-zinc-900 text-lg px-2 py-1 text-center whitespace-nowrap">{{ boss.name }}</p>
     <div class="flex flex-col sm:flex-row gap-1 text-center">
       <div :class="[boss.game === correct.game ? 'bg-green-900' : 'bg-red-900']" class="sm:min-w-[10rem] text-xl p-1">
         <p class="text-sm">Game</p>

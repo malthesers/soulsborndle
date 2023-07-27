c03cf3dc47d7321a400b162a8bcd71d79e446e27
malt9830 <malt9830@stud.kea.dk>
2023-07-27 04:58:33 +0200

    Added comments to KnownInfo html

--- src/components/panels/KnownInfo.vue
+++ src/components/panels/KnownInfo.vue
@@ -1,49 +1,54 @@
 <template>
   <div class="lg:sticky top-0 h-max sm:flex justify-center lg:block text-center bg-zinc-900 bg-opacity-60 p-2">
-      <div class="sm:inline-block lg:block mx-4">
-        <span class="text-sm">Game</span>
-        <Transition name="known" mode="out-in" class="h-10 whitespace-nowrap text-2xl">
-          <p :key="known.game">{{ known.game }}</p>
+    <!-- Known game -->
+    <div class="sm:inline-block lg:block mx-4">
+      <span class="text-sm">Game</span>
+      <Transition name="known" mode="out-in" class="h-10 whitespace-nowrap text-2xl">
+        <p :key="known.game">{{ known.game }}</p>
+      </Transition>
+    </div>
+    <!-- Known health -->
+    <div class="sm:inline-block lg:block mx-4">
+      <span class="text-sm">Health</span>
+      <p v-if="known.health">{{ known.health.toLocaleString() }}</p>
+      <p v-else class="h-10 whitespace-nowrap text-2xl">
+        <Transition name="known" mode="out-in">
+          <span :key="known.healthMin">{{ known.healthMin.toLocaleString() }}</span>
         </Transition>
-      </div>
-      <div class="sm:inline-block lg:block mx-4">
-        <span class="text-sm">Health</span>
-        <p v-if="known.health">{{ known.health.toLocaleString() }}</p>
-        <p v-else class="h-10 whitespace-nowrap text-2xl">
-          <Transition name="known" mode="out-in">
-            <span :key="known.healthMin">{{ known.healthMin.toLocaleString() }}</span>
-          </Transition>
-          <span class="px-2">-</span>
-          <Transition name="known" mode="out-in">
-            <span :key="known.healthMax">{{ known.healthMax.toLocaleString() }}</span>
-          </Transition>
-        </p>
-      </div>
-      <div class="sm:inline-block lg:block mx-4">
-        <span class="text-sm">Souls</span>
-        <p v-if="known.souls">{{ known.souls.toLocaleString() }}</p>
-        <p v-else class="h-10 whitespace-nowrap text-2xl">
-          <Transition name="known" mode="out-in">
-            <span :key="known.soulsMin">{{ known.soulsMin.toLocaleString() }}</span>
-          </Transition>
-          <span class="px-2">-</span>
-          <Transition name="known" mode="out-in">
-            <span :key="known.soulsMax">{{ known.soulsMax.toLocaleString() }}</span>
-          </Transition>
-        </p>
-      </div>
-      <div class="sm:inline-block lg:block mx-4">
-        <span class="text-sm">Weaknesses</span>
-        <TransitionGroup name="known" tag="div" class="h-10 flex flex-row justify-center gap-1">
-          <DamageBox v-for="weakness in known.weaknesses" :key="weakness" :damageType="weakness"/>
-        </TransitionGroup>
-      </div>
-      <div class="sm:inline-block lg:block mx-4">
-        <span class="text-sm">Resistances</span>
-        <TransitionGroup name="known" tag="div" class="h-10 flex flex-row justify-center gap-1">
-          <DamageBox v-for="resistance in known.resistances" :key="resistance" :damageType="resistance"/>
-        </TransitionGroup>
-      </div>
+        <span class="px-2">-</span>
+        <Transition name="known" mode="out-in">
+          <span :key="known.healthMax">{{ known.healthMax.toLocaleString() }}</span>
+        </Transition>
+      </p>
+    </div>
+    <!-- Known souls -->
+    <div class="sm:inline-block lg:block mx-4">
+      <span class="text-sm">Souls</span>
+      <p v-if="known.souls">{{ known.souls.toLocaleString() }}</p>
+      <p v-else class="h-10 whitespace-nowrap text-2xl">
+        <Transition name="known" mode="out-in">
+          <span :key="known.soulsMin">{{ known.soulsMin.toLocaleString() }}</span>
+        </Transition>
+        <span class="px-2">-</span>
+        <Transition name="known" mode="out-in">
+          <span :key="known.soulsMax">{{ known.soulsMax.toLocaleString() }}</span>
+        </Transition>
+      </p>
+    </div>
+    <!-- Known weaknesses -->
+    <div class="sm:inline-block lg:block mx-4">
+      <span class="text-sm">Weaknesses</span>
+      <TransitionGroup name="known" tag="div" class="h-10 flex flex-row justify-center gap-1">
+        <DamageBox v-for="weakness in known.weaknesses" :key="weakness" :damageType="weakness"/>
+      </TransitionGroup>
+    </div>
+    <!-- Known restistances -->
+    <div class="sm:inline-block lg:block mx-4">
+      <span class="text-sm">Resistances</span>
+      <TransitionGroup name="known" tag="div" class="h-10 flex flex-row justify-center gap-1">
+        <DamageBox v-for="resistance in known.resistances" :key="resistance" :damageType="resistance"/>
+      </TransitionGroup>
+    </div>
   </div>
 </template>
 

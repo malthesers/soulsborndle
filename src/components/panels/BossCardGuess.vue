3b8cdc8148f6d3ba58e9e4babe3bb2f173984885
malt9830 <malt9830@stud.kea.dk>
2023-07-27 05:00:02 +0200

    Added comments to BossCardGuess

--- src/components/panels/BossCardGuess.vue
+++ src/components/panels/BossCardGuess.vue
@@ -2,24 +2,29 @@
   <div>
     <p class="bg-zinc-900 text-lg px-2 py-1 text-center whitespace-nowrap">{{ boss.name }}</p>
     <div class="flex flex-col sm:flex-row gap-1 text-center">
+      <!-- Guessed boss game -->
       <div :class="[boss.game === correct.game ? 'bg-green-900' : 'bg-red-900']" class="sm:min-w-[10rem] text-xl p-1">
         <p class="text-sm">Game</p>
         <p class="leading-[2.2]">{{ boss.game }}</p>
       </div>
+      <!-- Guessed boss health -->
       <div :class="[boss.health === correct.health ? 'bg-green-900' : 'bg-red-900']" class="sm:min-w-[5rem] text-xl p-1">
         <p class="text-sm">Health</p>
         <p class="leading-[2.2]">{{ boss.health.toLocaleString() }}</p>
       </div>
+      <!-- Guessed boss souls -->
       <div :class="[boss.souls === correct.souls ? 'bg-green-900' : 'bg-red-900']" class="sm:min-w-[5rem] text-xl p-1">
         <p class="text-sm">Souls</p>
         <p class="leading-[2.2]">{{ boss.souls.toLocaleString() }}</p>
       </div>
+      <!-- Guessed boss weaknesses -->
       <div :class="weakMatch" class="sm:min-w-[8rem] w-full p-1">
         <p class="text-sm">Weaknesses</p>
         <div class="h-11 flex flex-row justify-center gap-1">
           <DamageBox v-for="weakness in boss.weaknesses" :key="weakness" :damageType="weakness"/>
         </div>
       </div>
+      <!-- Guessed boss resistances -->
       <div :class="resMatch" class="sm:min-w-[8rem] w-full p-1">
         <p class="text-sm">Resistances</p>
         <div class="h-11 flex flex-row justify-center gap-1">

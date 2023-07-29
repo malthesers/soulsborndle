ca77f9453b0d8c19786a96362916eb02314aec3f
malt9830 <malt9830@stud.kea.dk>
2023-07-29 05:10:33 +0200

    Made known labels <p> instead of <span>

--- src/components/panels/KnownInfo.vue
+++ src/components/panels/KnownInfo.vue
@@ -2,14 +2,14 @@
   <div class="lg:sticky top-0 h-max sm:flex justify-center lg:block text-center bg-zinc-900 bg-opacity-60 p-2">
     <!-- Known game -->
     <div class="sm:inline-block lg:block mx-4">
-      <span class="text-sm">Game</span>
+      <p class="text-sm">Game</p>
       <Transition name="known" mode="out-in" class="h-11 whitespace-nowrap text-2xl">
         <p :key="known.game">{{ known.game }}</p>
       </Transition>
     </div>
     <!-- Known health -->
     <div class="sm:inline-block lg:block mx-4">
-      <span class="text-sm">Health</span>
+      <p class="text-sm">Health</p>
       <p v-if="known.health" class="text-2xl">{{ known.health.toLocaleString() }}</p>
       <p v-else class="h-11 whitespace-nowrap text-2xl">
         <Transition name="known" mode="out-in">
@@ -23,7 +23,7 @@
     </div>
     <!-- Known souls -->
     <div class="sm:inline-block lg:block mx-4">
-      <span class="text-sm">Souls</span>
+      <p class="text-sm">Souls</p>
       <p v-if="known.souls" class="text-2xl">{{ known.souls.toLocaleString() }}</p>
       <p v-else class="h-11 whitespace-nowrap text-2xl">
         <Transition name="known" mode="out-in">
@@ -37,14 +37,14 @@
     </div>
     <!-- Known weaknesses -->
     <div class="sm:inline-block lg:block mx-4">
-      <span class="text-sm">Weaknesses</span>
+      <p class="text-sm">Weaknesses</p>
       <TransitionGroup name="known" tag="div" class="h-11 flex flex-row justify-center gap-1">
         <DamageBox v-for="weakness in known.weaknesses" :key="weakness" :damageType="weakness"/>
       </TransitionGroup>
     </div>
     <!-- Known restistances -->
     <div class="sm:inline-block lg:block mx-4">
-      <span class="text-sm">Resistances</span>
+      <p class="text-sm">Resistances</p>
       <TransitionGroup name="known" tag="div" class="h-11 flex flex-row justify-center gap-1">
         <DamageBox v-for="resistance in known.resistances" :key="resistance" :damageType="resistance"/>
       </TransitionGroup>

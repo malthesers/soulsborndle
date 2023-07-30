5e2a99e53ef98ac0338371d388561800e270d46e
malt9830 <malt9830@stud.kea.dk>
2023-07-30 07:12:03 +0200

    Further fix known box responsiveness

--- src/components/panels/KnownInfo.vue
+++ src/components/panels/KnownInfo.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="lg:sticky top-0 h-max sm:flex sm:gap-4 justify-center lg:block text-center bg-zinc-900 bg-opacity-60 p-2">
     <!-- Known game -->
-    <div class="sm:inline-block lg:block">
+    <div class="sm:inline-block lg:block mb-2 sm:mb-0">
       <p class="text-sm">Game</p>
-      <Transition name="known" mode="out-in" class="lg:h-11 whitespace-nowrap text-xl lg:text-2xl">
+      <Transition name="known" mode="out-in" class="md:h-11 sm:leading-[2.75rem] whitespace-nowrap text-xl lg:text-2xl">
         <p :key="known.game">{{ known.game }}</p>
       </Transition>
     </div>
     <!-- Known health & souls container -->
-    <div class="grid grid-cols-2 sm:flex sm:gap-4 lg:block">
+    <div class="grid grid-cols-2 sm:flex sm:gap-4 lg:block mb-2 sm:mb-0">
       <!-- Known health -->
       <div class="sm:inline-block lg:block">
         <p class="text-sm">Health</p>
-        <p v-if="known.health" class="text-xl lg:text-2xl">{{ known.health.toLocaleString() }}</p>
-        <p v-else class="lg:h-11 whitespace-nowrap text-lg xs:text-xl lg:text-2xl">
+        <p v-if="known.health" class="md:h-11 sm:leading-[2.75rem] text-lg xs:text-xl lg:text-2xl">{{ known.health.toLocaleString() }}</p>
+        <p v-else class="md:h-11 sm:leading-[2.75rem] whitespace-nowrap text-lg xs:text-xl lg:text-2xl">
           <Transition name="known" mode="out-in">
             <span :key="known.healthMin">{{ known.healthMin.toLocaleString() }}</span>
           </Transition>
@@ -26,8 +26,8 @@
       <!-- Known souls -->
       <div class="sm:inline-block lg:block">
         <p class="text-sm">Souls</p>
-        <p v-if="known.souls" class="text-xl lg:text-2xl">{{ known.souls.toLocaleString() }}</p>
-        <p v-else class="lg:h-11 whitespace-nowrap text-lg xs:text-xl lg:text-2xl">
+        <p v-if="known.souls" class="md:h-11 sm:leading-[2.75rem] text-lg xs:text-xl lg:text-2xl">{{ known.souls.toLocaleString() }}</p>
+        <p v-else class="md:h-11 sm:leading-[2.75rem] whitespace-nowrap text-lg xs:text-xl lg:text-2xl">
           <Transition name="known" mode="out-in">
             <span :key="known.soulsMin">{{ known.soulsMin.toLocaleString() }}</span>
           </Transition>

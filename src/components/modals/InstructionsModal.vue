c5b1eedfdf959e5499e5603b979afa4eaeee3a5f
malt9830 <malt9830@stud.kea.dk>
2023-07-31 09:55:28 +0200

    InstructionsModal damage icons no longer hover

--- src/components/modals/InstructionsModal.vue
+++ src/components/modals/InstructionsModal.vue
@@ -15,7 +15,7 @@
           <p class="mb-2">Arcane and Bolt are treated as Magic and Lightning, while Blunt and Pierce are treated Blunt and Thrust.</p>
           <div class="mx-auto mb-4 xs:w-full grid grid-cols-2 xs:grid-cols-3 gap-2">
             <p v-for="damageType in damageTypes" :key="damageType" class="flex gap-2 last:mx-auto last:col-span-2 xs:last:m-0 xs:last:col-span-1">
-              <DamageBox :damageType="damageType"/>
+              <DamageBox :damageType="damageType" :hover="false"/>
               <span class="capitalize my-auto">{{ damageType }}</span>
             </p>
           </div>

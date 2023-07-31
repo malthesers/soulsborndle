daaeed65ed107fd701590c2cc734dbc156a74815
malt9830 <malt9830@stud.kea.dk>
2023-07-31 09:50:51 +0200

    Added props to DamageBox to disable hover desc

--- src/components/layout/DamageBox.vue
+++ src/components/layout/DamageBox.vue
@@ -1,12 +1,16 @@
 <template>
-  <span class="relative grid w-11 h-11">
-    <img class="w-full m-auto p-1 object-cover border-zinc-900 border-4 rounded shadow-inner-dmg peer" :src="`icons/${damageType}.svg`" :alt="damageType">
-    <div class="absolute bottom-full left-1/2 bg-zinc-900 mb-1 px-2 py-1 rounded capitalize transform -translate-x-1/2 pointer-events-none duration-200 opacity-0 peer-hover:opacity-100">{{ damageType }}</div>
+  <span class="relative grid w-11 h-11 border-4 border-zinc-900 rounded shadow-inner-dmg group">
+    <img class="w-full h-full m-auto p-1 object-cover" :src="`icons/${damageType}.svg`" :alt="damageType">
+    <div v-if="hover" class="absolute -top-[110%] left-1/2 bg-zinc-700 px-2 py-1 rounded capitalize transform -translate-x-1/2 pointer-events-none duration-200 opacity-0 group-hover:opacity-100">{{ damageType }}</div>
   </span>
 </template>
 
 <script setup>
 const props = defineProps({
   damageType: String,
+  hover: {
+    type: Boolean,
+    default: true
+  }
 })
 </script>

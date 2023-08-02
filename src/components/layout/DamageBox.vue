9052eaf9c397aaad49c35715f7ab27efe3a32a0e
malt9830 <malt9830@stud.kea.dk>
2023-08-02 16:05:16 +0200

    Removed border from damage icons

--- src/components/layout/DamageBox.vue
+++ src/components/layout/DamageBox.vue
@@ -1,6 +1,7 @@
 <template>
-  <span class="relative grid place-content-center w-11 h-11 border-2 border-zinc-900 shadow-inner-dmg group">
-    <img class="w-7" :src="`icons/${damageType}.svg`" :alt="damageType">
+  <!-- <span class="relative grid place-content-center w-11 h-11 border-2 border-zinc-900 rounded shadow-inner-dmg group"> -->
+  <span class="relative grid w-11 h-11 rounded shadow-inner-dmg group">
+    <img class="w-8 m-auto" :src="`icons/${damageType}.svg`" :alt="damageType">
     <div v-if="hover" class="absolute -top-[100%] left-1/2 bg-zinc-700 px-2 py-1 rounded capitalize transform -translate-x-1/2 pointer-events-none duration-200 opacity-0 group-hover:opacity-100">{{ damageType }}</div>
   </span>
 </template>

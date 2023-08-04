b343906654346e81877aa1ab6d9a0f5f19fa2b24
malt9830 <malt9830@stud.kea.dk>
2023-08-04 13:51:47 +0200

    Added GitHub icon to header

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,7 +1,10 @@
 <template>
   <header>
-    <div class="bg-zinc-900 py-3 px-4 sm:py-4 sm:px-6">
-      <h1 class="text-xl xs:text-2xl sm:text-3xl text-left">Soulsborndle</h1>
+    <div class="flex flex-rows bg-zinc-900 py-3 px-4 sm:py-4 sm:px-6">
+      <h1 class="text-xl xs:text-2xl sm:text-3xl text-left my-auto leading-none">Soulsborndle</h1>
+      <a href="https://github.com/malthesers/soulsborndle" class="inline-block ml-auto w-7 xs:w-8 sm:w-9 h-7 xs:h-8 sm:h-9 duration-200 hover:opacity-50">
+        <img class="" src="src/assets/github-icon.svg">
+      </a>
     </div>
     <GateOrnament class="transform -scale-y-100"/>
   </header>

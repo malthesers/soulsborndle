ea43e207783e7e96d2d0bfae2c408f75848e3796
malt9830 <malt9830@stud.kea.dk>
2023-08-04 15:28:29 +0200

    Updated GitHub link

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -2,7 +2,7 @@
   <header>
     <div class="flex flex-rows bg-zinc-900 py-3 px-4 sm:py-4 sm:px-6">
       <h1 class="text-xl xs:text-2xl sm:text-3xl text-left my-auto leading-none">Soulsborndle</h1>
-      <a href="https://github.com/malthesers/soulsborndle" class="inline-block ml-auto w-7 xs:w-8 sm:w-9 h-7 xs:h-8 sm:h-9 duration-200 hover:opacity-50">
+      <a href="https://github.com/malthesers/soulsborndle" target="_blank" rel="noopener noreferrer" class="inline-block ml-auto w-7 xs:w-8 sm:w-9 h-7 xs:h-8 sm:h-9 duration-200 hover:opacity-50">
         <img class="" src="src/assets/github-icon.svg">
       </a>
     </div>

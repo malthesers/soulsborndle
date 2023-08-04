92ed522eacac85fe26b7eeca8249299ee29d9776
malt9830 <malt9830@stud.kea.dk>
2023-08-04 12:49:28 +0200

    Added transition for clearing records

--- src/components/modals/RecordsModal.vue
+++ src/components/modals/RecordsModal.vue
@@ -5,38 +5,45 @@
         <div class="overflow-auto w-full max-w-xl max-h-[75%] mx-auto bg-black p-4 text-center md:text-lg cursor-auto">
           <p class="text-3xl">Records</p>
           <p class="mb-2">Listed below are your 10 fastest victories sorted by amount of guesses. On the right, you see the games chosen when you set the record.</p>
-          <p v-if="records.length === 0" class="mb-2 font-bold">You have not guessed a boss yet.</p>
-          <table v-else class="w-full text-left text-base mb-2">
-            <thead>
-              <tr>
-                <th class="hidden xs:table-cell px-2 text-center">#</th>
-                <th class="hidden xs:table-cell">Boss</th>
-                <th v-for="(game, key) in games" :key="key"
-                  class="hidden xs:table-cell text-center w-9 border-l-4 border-black"
-                >{{ game }}</th>
-              </tr>
-            </thead>
-            <tbody v-for="record in records" :key="`${record.guesses}-${record.name}`" class="bg-zinc-900 border-black border-b-8">
-              <tr>
-                <td class="px-2 text-center">{{ record.guesses }}</td>
-                <td class="xs:hidden" colspan="5">{{ record.name }}</td>
-                <td class="hidden xs:table-cell" colspan="1">{{ record.name }}</td>
-                <td v-for="(game, key) in games" :key="key"
-                  :class="[record.games[key].isChosen ? 'bg-green-900' : 'bg-red-900']"
-                  class="hidden xs:table-cell border-l-4 border-black"
-                ></td>
-              </tr>
-              <!-- Mobile row -->
-              <tr class="xs:hidden">
-                <td
-                  v-for="(game, key) in games" :key="key"
-                  :class="[record.games[key].isChosen ? 'bg-green-900' : 'bg-red-900']"
-                  class="w-1/6 text-center"
-                ><span>{{ game }}</span></td>
-              </tr>
-            </tbody>
-          </table>
-          <div class="flex gap-4 justify-center">
+          <Transition name="recordsmessage">
+            <p v-if="records.length === 0" class="font-bold overflow-hidden">You have not guessed a boss yet.</p>
+          </Transition>
+          <Transition name="records">
+            <div v-if="records.length !== 0" class="overflow-hidden max-h-96">
+              <table class="w-full text-left text-base mb-2">
+                <thead>
+                  <tr>
+                    <th class="hidden xs:table-cell px-2 text-center">#</th>
+                    <th class="hidden xs:table-cell">Boss</th>
+                    <th v-for="(game, key) in games" :key="key"
+                      class="hidden xs:table-cell text-center w-9 border-l-4 border-black"
+                    >{{ game }}</th>
+                  </tr>
+                </thead>
+                <tbody v-for="record in records" :key="`${record.guesses}-${record.name}`" class="bg-zinc-900 border-black border-b-8">
+                  <!-- Desktop row -->
+                  <tr>
+                    <td class="px-2 text-center">{{ record.guesses }}</td>
+                    <td class="xs:hidden" colspan="5">{{ record.name }}</td>
+                    <td class="hidden xs:table-cell" colspan="1">{{ record.name }}</td>
+                    <td v-for="(game, key) in games" :key="key"
+                      :class="[record.games[key].isChosen ? 'bg-green-900' : 'bg-red-900']"
+                      class="hidden xs:table-cell border-l-4 border-black"
+                    ></td>
+                  </tr>
+                  <!-- Mobile row -->
+                  <tr class="xs:hidden">
+                    <td
+                      v-for="(game, key) in games" :key="key"
+                      :class="[record.games[key].isChosen ? 'bg-green-900' : 'bg-red-900']"
+                      class="w-1/6 text-center"
+                    ><span>{{ game }}</span></td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </Transition>
+          <div class="flex gap-4 justify-center mt-2">
             <Button v-if="records.length !== 0" @click="$emit('resetRecords')" text="Clear records"/>
             <Button @click="$emit('hideRecords')" text="Close"/>
           </div>

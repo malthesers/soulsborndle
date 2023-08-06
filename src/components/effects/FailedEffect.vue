b854463551949629cdefaa2add13cdecd485d8c4
malt9830 <malt9830@stud.kea.dk>
2023-08-06 13:30:34 +0200

    Failed effect can be exit with enter or escape

--- src/components/effects/FailedEffect.vue
+++ src/components/effects/FailedEffect.vue
@@ -1,7 +1,7 @@
 <template>
   <Teleport to="body">
     <Transition name="failed">
-      <div v-if="wasFailed" @click="$emit('hideEffect')" class="fixed top-0 w-full h-screen bg-black bg-opacity-30 flex flex-col place-content-center cursor-pointer">
+      <div v-if="wasFailed" ref="failedEffect" @click="$emit('hideEffect')" @keydown.enter="$emit('hideEffect')" @keydown.esc="$emit('hideEffect')" tabindex="0" class="fixed top-0 w-full h-screen bg-black bg-opacity-30 flex flex-col place-content-center cursor-pointer">
         <div class="grid grid-cols-[1fr] grid-rows-[1fr] place-content-center py-12 vertical-fade text-4xl sm:text-5xl md:text-7xl text-center">
           <p class="col-[1/1] row-[1/1] z-20 whitespace-nowrap text-red-900 brightness-100 transform scale-y-[150%]">YOU FAILED</p>
         </div>
@@ -14,6 +14,12 @@
 const emits = defineEmits(['hideEffect'])
 const props = defineProps({
   wasFailed: Boolean
+})
+
+const failedEffect = ref(null)
+
+watch(failedEffect, (newValue) => {
+  if (newValue) newValue.focus()
 })
 </script>
 

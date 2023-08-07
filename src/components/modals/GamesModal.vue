949f85999f2231b5ba454c9b78be65e1fcb8dd8f
malt9830 <malt9830@stud.kea.dk>
2023-08-07 02:39:45 +0200

    Inversed order of green/red description in modal

--- src/components/modals/GamesModal.vue
+++ src/components/modals/GamesModal.vue
@@ -6,7 +6,7 @@
         <div class="overflow-auto w-full max-w-xl max-h-[75%] mx-auto bg-black p-4 text-center md:text-lg cursor-auto">
           <p class="text-3xl">Games</p>
           <p class="mb-2">Below you can choose the games whose bosses will be included amongst the possible bosses. Toggling a filter resets the game.</p>
-          <p class="mb-2">A <span class="font-bold text-red-900">red</span> background means the game will be <span class="font-bold">excluded</span> from the pool of bosses, while a <span class="font-bold text-green-900">green</span> background means the game will be <span class="font-bold">included.</span></p>
+          <p class="mb-2">A <span class="font-bold text-green-700">green</span> background means the game will be <span class="font-bold">included</span> in the pool of bosses, while a <span class="font-bold text-red-700">red</span> background means the game will be <span class="font-bold">excluded.</span></p>
           <div class="grid grid-cols-2 gap-4 mb-2">
             <Button v-for="(game, key) in chosenGames"
               :key="game.name"

534610638b47cb7934f0970f49e88ff165ee2747
malt9830 <malt9830@stud.kea.dk>
2023-08-09 21:18:52 +0200

    Updated KnownInfo width to support health/souls

--- src/components/panels/KnownInfo.vue
+++ src/components/panels/KnownInfo.vue
@@ -1,19 +1,19 @@
 <template>
-  <div class="lg:sticky top-0 h-max md:flex md:gap-4 justify-center lg:block text-center bg-zinc-900 bg-opacity-60 p-2">
+  <div class="lg:sticky top-0 h-max sm:flex sm:gap-8 justify-center lg:block text-center bg-zinc-900 bg-opacity-60 p-2">
     <!-- Known game -->
     <div class="sm:inline-block lg:block mb-2 sm:mb-0">
       <p class="text-sm">Game</p>
-      <Transition name="known" mode="out-in" class="md:h-11 sm:leading-[2.2] whitespace-nowrap text-xl lg:text-2xl">
+      <Transition name="known" mode="out-in" class="sm:h-11 sm:leading-[2.2] whitespace-nowrap text-xl lg:text-2xl">
         <p :key="known.game">{{ known.game }}</p>
       </Transition>
     </div>
     <!-- Known health & souls container -->
-    <div class="grid grid-cols-2 md:flex md:gap-4 lg:block mb-2 sm:mb-0">
+    <div class="grid grid-cols-2 sm:flex sm:gap-8 lg:block mb-2 sm:mb-0">
       <!-- Known health -->
       <div class="sm:inline-block lg:block">
         <p class="text-sm">Health</p>
-        <p v-if="known.health" class="md:h-11 sm:leading-[2.2] text-lg xs:text-xl lg:text-2xl">{{ known.health.toLocaleString() }}</p>
-        <p v-else class="md:h-11 sm:leading-[2.2] text-lg xs:text-xl lg:text-2xl">
+        <p v-if="known.health" class="sm:h-11 sm:leading-[2.2] text-lg xs:text-xl lg:text-2xl">{{ known.health.toLocaleString() }}</p>
+        <p v-else class="sm:h-11 sm:leading-[2.2] text-lg xs:text-xl lg:text-2xl">
           <Transition name="known" mode="out-in">
             <span :key="known.health">{{ known.health.toLocaleString() }}</span>
           </Transition>
@@ -22,8 +22,8 @@
       <!-- Known souls -->
       <div class="sm:inline-block lg:block">
         <p class="text-sm">Souls</p>
-        <p v-if="known.souls" class="md:h-11 sm:leading-[2.2] text-lg xs:text-xl lg:text-2xl">{{ known.souls.toLocaleString() }}</p>
-        <p v-else class="md:h-11 sm:leading-[2.2] text-lg xs:text-xl lg:text-2xl">
+        <p v-if="known.souls" class="sm:h-11 sm:leading-[2.2] text-lg xs:text-xl lg:text-2xl">{{ known.souls.toLocaleString() }}</p>
+        <p v-else class="sm:h-11 sm:leading-[2.2] text-lg xs:text-xl lg:text-2xl">
           <Transition name="known" mode="out-in">
             <span :key="known.souls">{{ known.souls.toLocaleString() }}</span>
           </Transition>
@@ -31,7 +31,7 @@
       </div>
     </div>
     <!-- Known weaknesses & resistances container -->
-    <div class="grid grid-cols-2 md:flex md:gap-4 lg:block">
+    <div class="grid grid-cols-2 sm:flex sm:gap-8 lg:block">
       <!-- Known weaknesses -->
       <div class="sm:inline-block lg:block mb-4 sm:mb-0 lg:mb-4">
         <p class="text-sm">Weaknesses</p>

4af7655cfe7b02f97c046ff1b6422c7afe66dd95
malt9830 <malt9830@stud.kea.dk>
2023-08-26 20:46:58 +0200

    deployment

--- src/App.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <YharnamScreen/>
-  <Header/>
-  <Game/>
-</template>
--- src/components/ornaments/GateOrnament.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="bg-gradient-to-t from-zinc-900 h-6 xs:h-8 flex flex-row justify-between fill-white">
-    <GateOrnamentSingle class="transform -scale-x-100"/>
-    <GateOrnamentCenter/>
-    <GateOrnamentSingle/>
-  </div>
-</template>

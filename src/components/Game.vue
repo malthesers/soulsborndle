aa7477b4b2c78b5d6571630aa1063d00526f14a4
malt9830 <malt9830@stud.kea.dk>
2023-11-11 23:19:06 +0100

    Re-structured guesses panel

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -19,9 +19,11 @@
           :search="search"
           :wasGuessed="wasGuessed"
         />
-        <TransitionGroup :name="wasFailed ? 'failed' : 'guess'" tag="div" mode="out-in" class="flex flex-col gap-4 min-h-[40rem] sm:min-h-0">
-          <BossGuess v-for="boss in guessedBosses" :key="boss.name" :boss="boss" :correct="correct" class="failed-fade"/>
-        </TransitionGroup>
+        <GuessesPanel
+          :guessedBosses="guessedBosses"
+          :wasFailed="wasFailed"
+          :correct="correct"
+        />
       </div>
     </div>
     <!-- Effects -->
@@ -78,10 +80,8 @@ const oneGameChosen = computed(() => {
   return (chosenGamesCount === 1 ? true : false)
 })
 const noGamesChosen = computed(() => {
-  const gamesKeys = Object.keys(chosenGames.value)
-
-  // Return true if no game
-  return gamesKeys.every(key => {
+  // Return true if every game is not chosen
+  return Object.keys(chosenGames.value).every(key => {
     return chosenGames.value[key].isChosen === false
   })
 })
@@ -287,22 +287,3 @@ onMounted(() => {
   newGame()
 })
 </script>
-
-<style scoped>
-.failed-enter-active,
-.failed-leave-active {
-  transition: opacity 2500ms ease-in;
-}
-
-.guess-enter-active,
-.guess-leave-active {
-  transition: opacity 250ms ease-in;
-}
-
-.failed-enter-from,
-.failed-leave-to,
-.guess-enter-from,
-.guess-leave-to {
-  opacity: 0;
-}
-</style>
---
+++ src/components/panels/guesses/GuessesPanel.vue
@@ -0,0 +1,32 @@
+<template>
+  <TransitionGroup :name="wasFailed ? 'failed' : 'guess'" tag="div" mode="out-in" class="flex flex-col gap-4 min-h-[40rem] sm:min-h-0">
+    <BossGuess v-for="boss in guessedBosses" :key="boss.name" :boss="boss" :correct="correct" class="failed-fade"/>
+  </TransitionGroup>
+</template>
+
+<script setup>
+const props = defineProps({
+  guessedBosses: Array,
+  wasFailed: Boolean,
+  correct: Object,
+})
+</script>
+
+<style scoped>
+.failed-enter-active,
+.failed-leave-active {
+  transition: opacity 2500ms ease-in;
+}
+
+.guess-enter-active,
+.guess-leave-active {
+  transition: opacity 250ms ease-in;
+}
+
+.failed-enter-from,
+.failed-leave-to,
+.guess-enter-from,
+.guess-leave-to {
+  opacity: 0;
+}
+</style>

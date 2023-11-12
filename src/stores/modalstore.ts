7ddb0a207547ab6126f094c5b03eb6af849e9cb3
malt9830 <malt9830@stud.kea.dk>
2023-11-12 19:50:55 +0100

    Added open and close methods for modalStore

--- src/stores/modalstore.ts
+++ src/stores/modalstore.ts
@@ -2,26 +2,38 @@ import { defineStore } from 'pinia'
 import { type Ref } from 'vue'
 
 interface Modals {
-  [key:string]: boolean
+  instructions: boolean,
+  records: boolean,
+  bosses: boolean,
+  guessed: boolean,
+  failed: boolean,
 }
 
 export const useModalStore = defineStore('modalStore', () => {
-  const wasGuessed:Ref<boolean> = ref(false)
-  const wasFailed:Ref<boolean> = ref(false)
   const showModal:Ref<Modals> = ref({
     instructions: false,
     records: false,
-    bosses: false
+    bosses: false,
+    guessed: false,
+    failed: false
   })
   
   const modalOpen:ComputedRef<boolean> = computed(() => {
     return Object.values(showModal.value).some(modal => modal === true)
   })
 
+  function open(modal:keyof Modals) {
+    showModal.value[modal] = true
+  }
+
+  function close(modal:keyof Modals) {
+    showModal.value[modal] = false
+  }
+
   return {
-    wasGuessed,
-    wasFailed,
     showModal,
-    modalOpen
+    modalOpen,
+    open,
+    close
   }
 })

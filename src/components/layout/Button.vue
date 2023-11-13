8b40c3c5fab4cd93823d6c9ae7a35936667d5ffe
malt9830 <malt9830@stud.kea.dk>
2023-11-14 00:25:28 +0100

    Implemented TS in Button

--- src/components/layout/Button.vue
+++ src/components/layout/Button.vue
@@ -1,15 +1,17 @@
 <template>
   <button :class="{ 'focus-visible:bg-white focus-visible:text-zinc-900 hover:bg-white hover:text-zinc-900': hover }"
-    class="font-bold border-2 border-white sm:text-xl py-2 px-4 whitespace-nowrap duration-200 focus:outline-none">{{ text
-    }}</button>
+    class="font-bold border-2 border-white sm:text-xl py-2 px-4 whitespace-nowrap duration-200 focus:outline-none">
+    {{ text }}
+  </button>
 </template>
 
-<script setup>
-const props = defineProps({
-  text: String,
-  hover: {
-    type: Boolean,
-    default: true
-  }
+<script setup lang="ts">
+interface Props {
+  text: string,
+  hover?: boolean
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  hover: true
 })
 </script>

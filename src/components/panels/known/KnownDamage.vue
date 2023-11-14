69c407806721bd943e886cac949c2c5d9285ad5f
malt9830 <malt9830@stud.kea.dk>
2023-11-14 03:11:53 +0100

    Added KnownDamage to display known w/r

---
+++ src/components/panels/known/KnownDamage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div :class="{ 'lg:mb-2': value === 'weaknesses' }" class="sm:min-w-[8rem] sm:inline-block lg:block">
+    <p class="text-sm capitalize">{{ value }}</p>
+    <Transition name="known" mode="out-in">
+      <p v-if="damageList.length === 0 && !hasNo" class="h-11 sm:leading-[2.2] text-lg xs:text-xl lg:text-2xl">?</p>
+      <TransitionGroup v-else-if="damageList.length > 0" name="known" tag="div"
+        class="h-11 flex flex-row justify-center gap-1">
+        <DamageBox v-for="weakness in damageList" :key="weakness" :damageType="weakness" />
+      </TransitionGroup>
+      <p v-else class="h-11 sm:leading-[2.2] text-lg xs:text-xl lg:text-2xl">None</p>
+    </Transition>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { DamageType } from '@/interfaces';
+
+const props = defineProps<{
+  hasNo: boolean,
+  damageList: DamageType[],
+  value: 'weaknesses' | 'resistances'
+}>()
+</script>
--- src/components/panels/known/KnownPanel.vue
+++ src/components/panels/known/KnownPanel.vue
@@ -5,8 +5,8 @@
       <p class="text-sm">Game</p>
       <Transition name="known" mode="out-in">
         <p class="sm:min-w-[10rem] sm:h-11 sm:leading-[2.2] whitespace-nowrap text-xl lg:text-2xl"
-          :key="bossesStore.known.game">{{
-            bossesStore.known.game }}</p>
+          :key="bossesStore.known.game">
+          {{ bossesStore.known.game }}</p>
       </Transition>
     </div>
     <!-- Known health & souls container -->
@@ -19,31 +19,11 @@
     <!-- Known weaknesses & resistances container -->
     <div class="grid grid-cols-2 sm:flex sm:gap-1 lg:block">
       <!-- Known weaknesses -->
-      <div class="sm:min-w-[8rem] sm:inline-block lg:block lg:mb-2">
-        <p class="text-sm">Weaknesses</p>
-        <Transition name="known" mode="out-in">
-          <p v-if="bossesStore.known.weaknesses.length === 0 && !bossesStore.known.hasNo.weaknesses"
-            class="h-11 sm:leading-[2.2] text-lg xs:text-xl lg:text-2xl">?</p>
-          <TransitionGroup v-else-if="bossesStore.known.weaknesses.length > 0" name="known" tag="div"
-            class="h-11 flex flex-row justify-center gap-1">
-            <DamageBox v-for="weakness in bossesStore.known.weaknesses" :key="weakness" :damageType="weakness" />
-          </TransitionGroup>
-          <p v-else class="h-11 sm:leading-[2.2] text-lg xs:text-xl lg:text-2xl">None</p>
-        </Transition>
-      </div>
+      <KnownDamage :hasNo="bossesStore.known.hasNo.weaknesses" :damageList="bossesStore.known.weaknesses"
+        value="weaknesses" />
       <!-- Known restistances -->
-      <div class="sm:min-w-[8rem] sm:inline-block lg:block">
-        <p class="text-sm">Resistances</p>
-        <Transition name="known" mode="out-in">
-          <p v-if="bossesStore.known.resistances.length === 0 && !bossesStore.known.hasNo.resistances"
-            class="h-11 sm:leading-[2.2] text-lg xs:text-xl lg:text-2xl">?</p>
-          <TransitionGroup v-else-if="bossesStore.known.resistances.length > 0" name="known" tag="div"
-            class="h-11 flex flex-row justify-center gap-1">
-            <DamageBox v-for="resistance in bossesStore.known.resistances" :key="resistance" :damageType="resistance" />
-          </TransitionGroup>
-          <p v-else class="h-11 sm:leading-[2.2] text-lg xs:text-xl lg:text-2xl">None</p>
-        </Transition>
-      </div>
+      <KnownDamage :hasNo="bossesStore.known.hasNo.resistances" :damageList="bossesStore.known.resistances"
+        value="resistances" />
     </div>
   </div>
 </template>

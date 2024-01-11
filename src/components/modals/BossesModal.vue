95cc67da78e7885ec10140a5c0bd7326ed2624d0
malt9830 <malt9830@stud.kea.dk>
2024-01-11 16:34:35 +0100

    Wrapped BossesModal in ModalTemplate

--- src/components/modals/BossesModal.vue
+++ src/components/modals/BossesModal.vue
@@ -1,35 +1,32 @@
 <template>
-  <Teleport to="body">
-    <Transition name="modal">
-      <div v-if="modalStore.showing['bosses']" ref="bossesModal" tabindex="0" @click.self="hideGames"
-        class="fixed z-20 top-0 w-full h-dvh bg-black bg-opacity-30 flex flex-col place-content-center cursor-pointer p-2">
-        <!-- Game selector panel -->
-        <div class="overflow-auto w-full max-w-xl max-h-[80%] mx-auto bg-black p-4 text-center md:text-lg cursor-auto">
-          <p class="text-3xl">Bosses</p>
-          <p class="mb-2">Below you can choose the games whose bosses will be included.</p>
-          <p class="mb-2">Bloodborne does not include Chalice Dungeon bosses, and Elden Ring does not include field
-            bosses.</p>
-          <p class="mb-2">Toggling a filter resets the game.</p>
-          <p class="mb-2">A <span class="font-bold text-green-700">green</span> background means the game will be <span
-              class="font-bold">included</span> in the pool of bosses, while a <span
-              class="font-bold text-red-700">red</span> background means the game will be <span
-              class="font-bold">excluded.</span></p>
-          <div class="grid grid-cols-2 gap-4 mb-2">
-            <SoulsButton v-for="(game, key) in gamesStore.chosen" :key="key" :text="games[key]"
-              :class="game ? 'bg-green-900' : 'bg-red-900'" class="!px-2 hover:bg-opacity-50" :hover="false"
-              @click="toggleGame(key)" />
-          </div>
-          <!-- Error message -->
-          <Transition name="message">
-            <p v-if="showErrorMessage" :class="{ 'animate-pulse': showErrorMessageExtra }"
-              class="text-red-500 drop-shadow-red duration-300 overflow-hidden">You must choose at least 1 game.</p>
-          </Transition>
-          <!-- Buttons -->
-          <SoulsButton @click="hideGames" text="Close" class="mt-2" />
-        </div>
-      </div>
+  <ModalTemplate title="Bosses" :showModal="modalStore.showing['bosses']" :closeModal="hideGames">
+    <p class="mb-2">Below you can choose the games whose bosses will be included.</p>
+    <p class="mb-2">Bloodborne does not include Chalice Dungeon bosses, and Elden Ring does not include field
+      bosses.</p>
+    <p class="mb-2">Toggling a filter resets the game.</p>
+    <p class="mb-2">
+      <span>A </span>
+      <span class="font-bold text-green-700">green </span>
+      <span>background means the game will be </span>
+      <span class="font-bold">included </span>
+      <span>in the pool of bosses, while a </span>
+      <span class="font-bold text-red-700">red </span>
+      <span>background means the game will be </span>
+      <span class="font-bold">excluded.</span>
+    </p>
+    <div class="grid grid-cols-2 gap-4 mb-2">
+      <SoulsButton v-for="(game, key) in gamesStore.chosen" :key="key" :text="games[key]"
+        :class="game ? 'bg-green-900' : 'bg-red-900'" class="!px-2 hover:bg-opacity-50" :hover="false"
+        @click="toggleGame(key)" />
+    </div>
+    <!-- Error message -->
+    <Transition name="message">
+      <p v-if="showErrorMessage" :class="{ 'animate-pulse': showErrorMessageExtra }"
+        class="text-red-500 drop-shadow-red duration-300 overflow-hidden">You must choose at least 1 game.</p>
     </Transition>
-  </Teleport>
+    <!-- Buttons -->
+    <SoulsButton @click="hideGames" text="Close" class="mt-2" />
+  </ModalTemplate>
 </template>
 
 <script setup lang="ts">

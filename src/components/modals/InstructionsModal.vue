954fd2b802c7156d07f83472db339ca648882d7e
malt9830 <malt9830@stud.kea.dk>
2024-01-12 15:33:16 +0100

    Updated prettier settings

--- src/components/modals/InstructionsModal.vue
+++ src/components/modals/InstructionsModal.vue
@@ -10,8 +10,7 @@
       <span class="font-bold text-green-900">green </span>
       <span>completely correct.</span>
     </p>
-    <p class="mb-2">An arrow above health and souls indicates the correct value is higher, while an arrow below indicates
-      it is lower.</p>
+    <p class="mb-2">An arrow above health and souls indicates the correct value is higher, while an arrow below indicates it is lower.</p>
     <!-- Bosses information -->
     <p class="text-xl">Bosses</p>
     <p class="mb-2">
@@ -25,8 +24,7 @@
     </div>
     <!-- Damage type information -->
     <p class="text-xl">Damage Types</p>
-    <p class="mb-2">Arcane and Bolt are treated as Magic and Lightning, while Blunt and Pierce are treated Strike
-      and Thrust.</p>
+    <p class="mb-2">Arcane and Bolt are treated as Magic and Lightning, while Blunt and Pierce are treated Strike and Thrust.</p>
     <div class="mx-auto mb-4 xs:w-full grid grid-cols-2 xs:grid-cols-3 gap-2">
       <p v-for="damageType in bossesStore.damageTypes" :key="damageType" class="flex gap-2">
         <DamageBox :damageType="damageType" :hover="false" />
@@ -39,15 +37,15 @@
 </template>
 
 <script setup lang="ts">
-import type { Game } from '@/interfaces';
-import { useBossesStore } from '@/stores/bossesStore';
+import type { Game } from '@/interfaces'
+import { useBossesStore } from '@/stores/bossesStore'
 import { useModalStore } from '@/stores/modalStore'
 
 const bossesStore = useBossesStore()
 const modalStore = useModalStore()
 const instructionsModal: Ref<HTMLDivElement | null> = ref(null)
-const gamesDesktop: Ref<Game[]> = ref(['Demon\'s Souls', 'Bloodborne', 'Elden Ring', 'Dark Souls', 'Dark Souls II', 'Dark Souls III'])
-const gamesMobile: Ref<Game[]> = ref(['Demon\'s Souls', 'Dark Souls', 'Bloodborne', 'Dark Souls II', 'Elden Ring', 'Dark Souls III'])
+const gamesDesktop: Ref<Game[]> = ref(["Demon's Souls", 'Bloodborne', 'Elden Ring', 'Dark Souls', 'Dark Souls II', 'Dark Souls III'])
+const gamesMobile: Ref<Game[]> = ref(["Demon's Souls", 'Dark Souls', 'Bloodborne', 'Dark Souls II', 'Elden Ring', 'Dark Souls III'])
 
 function hideInstructions(): void {
   modalStore.close('instructions')

7a2e2027aec31ea9fc357dc3d257de3460a71161
malt9830 <malt9830@stud.kea.dk>
2024-01-28 05:05:31 +0100

    Removed plugin import in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 
-const plugin = require('tailwindcss/plugin')
-
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {

5a341143fcca831173edca108b333d09e428a465
malt9830 <malt9830@stud.kea.dk>
2024-01-29 11:58:43 +0100

    Changed yharnam-layer class to individual classes

--- src/components/layout/YharnamScreen.vue
+++ src/components/layout/YharnamScreen.vue
@@ -1,16 +1,10 @@
 <template>
-  <div class="fixed -z-10 top-0 w-full h-dvh filter brightness-50">
-    <div class="yharnam-layer bg-[20%_center] bg-[url('/src/assets/background/1_moon.svg')]"></div>
-    <div class="yharnam-layer bg-[5%_center] bg-[url('/src/assets/background/2_background.svg')]"></div>
-    <div class="yharnam-layer bg-center bg-[url('/src/assets/background/3_bg-light.svg')]"></div>
-    <div class="yharnam-layer bg-[30%_center] bg-[url('/src/assets/background/4_middleground.svg')]"></div>
-    <div class="yharnam-layer bg-center bg-[url('/src/assets/background/5_mg-light.svg')]"></div>
-    <div class="yharnam-layer bg-[10%_center] bg-[url('/src/assets/background/6_foreground.svg')]"></div>
+  <div class="fixed -z-10 top-0 w-full h-dvh filter brightness-50 *:absolute *:top-0 *:size-full *:bg-cover">
+    <div class="bg-[20%_center] bg-[url('/src/assets/background/1_moon.svg')]"></div>
+    <div class="bg-[5%_center] bg-[url('/src/assets/background/2_background.svg')]"></div>
+    <div class="bg-center bg-[url('/src/assets/background/3_bg-light.svg')]"></div>
+    <div class="bg-[30%_center] bg-[url('/src/assets/background/4_middleground.svg')]"></div>
+    <div class="bg-center bg-[url('/src/assets/background/5_mg-light.svg')]"></div>
+    <div class="bg-[10%_center] bg-[url('/src/assets/background/6_foreground.svg')]"></div>
   </div>
 </template>
-
-<style lang="postcss" scoped>
-.yharnam-layer {
-  @apply absolute top-0 size-full bg-cover;
-}
-</style>

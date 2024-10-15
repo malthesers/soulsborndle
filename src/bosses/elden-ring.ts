7806c7129f9f453650424a7258b10da29f45cfed
Malthe Kusk Lauritsen <malthekusk@gmail.com>
2024-10-15 10:55:31 +0200

    Added Shadow of the Erdtree bosses (#14)
    * Updated package
    
    * Added Shadow of the Erdtree bosses

--- src/bosses/elden-ring.ts
+++ src/bosses/elden-ring.ts
@@ -372,5 +372,254 @@ export const eldenRingBosses: EldenRingBoss[] = [
     souls: 10000,
     weaknesses: [],
     resistances: []
+  },
+  // Shadow of the Erdtree
+  // Gravesite Plain
+  {
+    name: 'Blackgaol Knight',
+    game: 'Elden Ring',
+    health: 14129,
+    souls: 70000,
+    weaknesses: ['lightning'],
+    resistances: ['fire', 'holy']
+  },
+  {
+    name: 'Chief Bloodfiend',
+    game: 'Elden Ring',
+    health: 10974,
+    souls: 80000,
+    weaknesses: ['slash'],
+    resistances: ['fire']
+  },
+  {
+    name: 'Demi-Human Swordmaster Onze',
+    game: 'Elden Ring',
+    health: 7441,
+    souls: 80000,
+    weaknesses: ['slash', 'fire'],
+    resistances: []
+  },
+  {
+    name: 'Divine Beast Dancing Lion',
+    game: 'Elden Ring',
+    health: 22571,
+    souls: 120000,
+    weaknesses: ['thrust', 'fire'],
+    resistances: ['magic', 'lightning', 'holy']
+  },
+  {
+    name: 'Rellana, Twin Moon Knight',
+    game: 'Elden Ring',
+    health: 29723,
+    souls: 240000,
+    weaknesses: ['thrust', 'lightning'],
+    resistances: ['slash', 'magic']
+  },
+  {
+    name: 'Red Bear',
+    game: 'Elden Ring',
+    health: 16365,
+    souls: 80000,
+    weaknesses: ['lightning'],
+    resistances: ['holy']
+  },
+  // Scadu Altus
+  {
+    name: 'Commander Gaius',
+    game: 'Elden Ring',
+    health: 33871,
+    souls: 230000,
+    weaknesses: ['holy'],
+    resistances: ['slash']
+  },
+  {
+    name: 'Golden Hippopotamus',
+    game: 'Elden Ring',
+    health: 33866,
+    souls: 200000,
+    weaknesses: ['slash', 'fire', 'lightning'],
+    resistances: []
+  },
+  {
+    name: 'Messmer the Impaler',
+    game: 'Elden Ring',
+    health: 38981,
+    souls: 400000,
+    weaknesses: ['slash'],
+    resistances: ['fire']
+  },
+  {
+    name: 'Base Serpent Messmer',
+    game: 'Elden Ring',
+    health: 38981,
+    souls: 400000,
+    weaknesses: ['slash', 'holy'],
+    resistances: ['fire']
+  },
+  {
+    name: 'Metyr, Mother of Fingers',
+    game: 'Elden Ring',
+    health: 43296,
+    souls: 420000,
+    weaknesses: ['slash'],
+    resistances: []
+  },
+  // Rauh Base
+  {
+    name: 'Divine Beast Dancing Lion (Ancient Ruins of Rauh)',
+    game: 'Elden Ring',
+    health: 32838,
+    souls: 180000,
+    weaknesses: ['thrust', 'fire'],
+    resistances: ['magic', 'lightning', 'holy']
+  },
+  {
+    name: 'Romina, Saint of the Bud',
+    game: 'Elden Ring',
+    health: 35798,
+    souls: 380000,
+    weaknesses: ['fire'],
+    resistances: ['slash']
+  },
+  // Cerulean Coast
+  {
+    name: 'Putrescent Knight',
+    game: 'Elden Ring',
+    health: 20612,
+    souls: 220000,
+    weaknesses: ['holy'],
+    resistances: ['slash', 'thrust']
+  },
+  // Jagged Peak
+  {
+    name: 'Ancient Dragon Senessax',
+    game: 'Elden Ring',
+    health: 45551,
+    souls: 200000,
+    weaknesses: ['thrust'],
+    resistances: ['lightning']
+  },
+  {
+    name: 'Bayle the Dread',
+    game: 'Elden Ring',
+    health: 41612,
+    souls: 490000,
+    weaknesses: ['thrust'],
+    resistances: []
+  },
+  {
+    name: 'Jagged Peak Drake (Foothills)',
+    game: 'Elden Ring',
+    health: 31941,
+    souls: 90000,
+    weaknesses: ['thrust'],
+    resistances: []
+  },
+  {
+    name: 'Jagged Peak Drake',
+    game: 'Elden Ring',
+    health: 39377,
+    souls: 120000,
+    weaknesses: ['thrust'],
+    resistances: []
+  },
+  // Abyssal Woods
+  {
+    name: 'Jori, Elder Inquisitor',
+    game: 'Elden Ring',
+    health: 30506,
+    souls: 260000,
+    weaknesses: ['slash', 'fire'],
+    resistances: []
+  },
+  {
+    name: 'Midra, Lord of Frenzied Flame',
+    game: 'Elden Ring',
+    health: 47171,
+    souls: 410000,
+    weaknesses: ['slash'],
+    resistances: ['fire']
+  },
+  // Scaduview
+  {
+    name: 'Scadutree Avatar (Phase 1)',
+    game: 'Elden Ring',
+    health: 7560,
+    souls: 260000,
+    weaknesses: ['strike', 'fire'],
+    resistances: ['magic', 'holy']
+  },
+  {
+    name: 'Scadutree Avatar (Phase 2)',
+    game: 'Elden Ring',
+    health: 13608,
+    souls: 260000,
+    weaknesses: ['strike', 'fire'],
+    resistances: ['magic', 'holy']
+  },
+  {
+    name: 'Scadutree Avatar (Phase 3)',
+    game: 'Elden Ring',
+    health: 13608,
+    souls: 260000,
+    weaknesses: ['strike', 'fire'],
+    resistances: ['magic', 'holy']
+  },
+  // Enir-Ilim
+  {
+    name: 'Leda and Allies (Redmane Freyja)',
+    game: 'Elden Ring',
+    health: 15101,
+    souls: 300000,
+    weaknesses: [],
+    resistances: []
+  },
+  {
+    name: 'Leda and Allies (Hornsent)',
+    game: 'Elden Ring',
+    health: 9992,
+    souls: 300000,
+    weaknesses: [],
+    resistances: []
+  },
+  {
+    name: 'Leda and Allies (Moore)',
+    game: 'Elden Ring',
+    health: 14615,
+    souls: 300000,
+    weaknesses: [],
+    resistances: []
+  },
+  {
+    name: 'Leda and Allies (Dryleaf Dane)',
+    game: 'Elden Ring',
+    health: 15911,
+    souls: 300000,
+    weaknesses: [],
+    resistances: []
+  },
+  {
+    name: 'Leda and Allies (Needle Knight Leda)',
+    game: 'Elden Ring',
+    health: 17256,
+    souls: 300000,
+    weaknesses: [],
+    resistances: []
+  },
+  {
+    name: 'Promised Consort Radahn',
+    game: 'Elden Ring',
+    health: 46134,
+    souls: 500000,
+    weaknesses: ['holy'],
+    resistances: []
+  },
+  {
+    name: 'Radahn, Consort of Miquella',
+    game: 'Elden Ring',
+    health: 46134,
+    souls: 500000,
+    weaknesses: ['thrust'],
+    resistances: []
   }
 ]
